7d8dd71a8f0ef3cb692c6624f2a5361fb8675923
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2024-11-03 08:31:30 +0200

    fix: conv tuning [skip ci]

--- service/converter/service.go
+++ service/converter/service.go
@@ -291,6 +291,9 @@ func (c svc) handleHtml(src string, evt *pb.CloudEvent) (err error) {
 				if strings.TrimSpace(txt) == "read more" {
 					return true
 				}
+				if strings.TrimSpace(txt) == "web reader" {
+					return true
+				}
 				if strings.TrimSpace(txt) == "sign up" {
 					return true
 				}
