e07ceb5f55acfcca920e3c87bbf1a04de541e8f5
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2024-12-10 19:46:43 +0200

    feat: use pub rest api [skip ci]

--- model/metadata.go
+++ model/metadata.go
@@ -2,12 +2,3 @@ package model
 
 const KeyGroupId = "x-awakari-group-id"
 const KeyUserId = "x-awakari-user-id"
-
-const KeyCeGroupId = "awakarigroupid"
-const KeyCeUserId = "awakariuserid"
-const KeyCePubTime = "awkpubtime"
-
-const KeyToGroupId = "awktogroupid"
-const KeyToUserId = "awktouserid"
-
-const ValTypeLimitReached = "com_awakari_api_permits_exhausted"
